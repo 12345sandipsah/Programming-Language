f983c7aad4679313c3208f7398a93facf1fca471
Shyam Kumar Chaurasiya <chaurasiyashyam91@gmail.com>
2025-09-05 22:42:54 +0530

    Area of rectangle

---
+++ AreaOfRectangle.java
@@ -0,0 +1,8 @@
+public class AreaOfRectangle {
+    public static void main(String[] args) {
+        int length = 5;
+        int width = 3;
+        int area = length * width;
+        System.out.println("Area of the rectangle: " + area);
+    }
+}
